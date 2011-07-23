9497d69b8a248a003178013a79782563b8ab8bcb
Cenk Çivici <cenk.civici@gmail.com>
2011-07-24 00:42:41 +0300

    cC

--- BigPipe/src/main/com/prime/bigpipe/web/component/DefaultComponentHandlerExecutor.java
+++ BigPipe/src/main/com/prime/bigpipe/web/component/DefaultComponentHandlerExecutor.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import com.prime.bigpipe.web.component.handler.ComponentHandler;
 
 @Service
-public class DefaultComponentHandlerExecutor {
+public class DefaultComponentHandlerExecutor implements ComponentHandlerExecutor {
 
 	@Autowired
 	private ComponentRenderer componentRenderer;
